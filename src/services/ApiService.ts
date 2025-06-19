0175ce26717445995de4ebcd0eab73f28c702b6c
Senura Chamod <senoocoding@gmail.com>
2025-06-20 01:35:11 +0530

    MOD: CONFIG: Updated backend api Url. (from localhost->https://beard.kodfec.com/)

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -6,7 +6,7 @@ class ApiService {
   
   private constructor() {
     this.axiosInstance = axios.create({
-      baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api',
+      baseURL: import.meta.env.VITE_API_BASE_URL || 'https://beard.kodfec.com/api',
       headers: {
         'Content-Type': 'application/json'
       },
