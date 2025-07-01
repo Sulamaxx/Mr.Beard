8006060e4f943b070c19f59841616b36ad21eb72
Senura Chamod <senoocoding@gmail.com>
2025-07-01 21:02:13 +0530

    MOD: Updated and fixed the About page text size and image positions.
    Refactored the About Us page to improve layout and responsiveness, including restructuring the Vision and Mission sections to use image elements instead of background images. Updated mission.png and vision.png images, removed about_image.png, and adjusted SCSS for better text sizing and commented out unused image container styles.

--- src/pages/aboutUs/AboutUs.tsx
+++ src/pages/aboutUs/AboutUs.tsx
@@ -10,7 +10,7 @@ const AboutUs: React.FC = () => {
     <div className="about-us">
       <Container>
         {/* About Us Header */}
-        <Row className="mb-lg-5">
+        <Row>
           <Col xs={12} className="text-center mt-5">
             <h1 className="section-title">Meet the Founder</h1>
           </Col>
@@ -18,58 +18,70 @@ const AboutUs: React.FC = () => {
 
         {/* Title Section with Image */}
         <Row className="mb-md-5 align-items-start">
-          <Col lg={5} md={6} sm={12} className="mb-4 mb-md-0">
-            <div className="product-image-container">
+          <Col className="mb-4 mb-md-0 col-sm-auto">
+            <div className="product-image-containe">
               <img
                 src={ownerImage}
-                alt="Beard Wax Product"
+                alt="The Company Founder"
                 className="img-fluid product-image"
+                width={300}
               />
             </div>
           </Col>
-          <Col lg={7} md={6} sm={12}>
-            <p className="section-text text-md-start">
-            Dilshan T Amarasinghe is a dynamic entrepreneur, Visiting Lecturer, and SME Business Consultant with a Entrepreneurship and Business Management background from the University of Sri Jayewardenepura. As the owner of four brands, he is dedicated to empowering businesses and redefining men’s grooming in Sri Lanka.
- <br/><br/>
-At Mr. Beard, we believe grooming is more than just style—it’s about confidence, self-care, and making a difference. Join us in transforming men’s grooming while supporting a truly important cause.
+          <Col lg={8} md={6} sm={12}>
+            <p className="section-text text-md-start mt-md-5">
+              Dilshan T Amarasinghe is a dynamic entrepreneur, Visiting
+              Lecturer, and SME Business Consultant with a Entrepreneurship and
+              Business Management background from the University of Sri
+              Jayewardenepura. As the owner of four brands, he is dedicated to
+              empowering businesses and redefining men’s grooming in Sri Lanka.
+               <br />
+              <br />
+              At Mr. Beard, we believe grooming is more than just style—it’s
+              about confidence, self-care, and making a difference. Join us in
+              transforming men’s grooming while supporting a truly important
+              cause.
             </p>
           </Col>
         </Row>
 
         {/* Vision Section */}
         <Row>
-        <h1 className="text-uppercase text-center text-md-start mt-5 section-subtitle">
-            Vision
-          </h1>
-          <Col md={7} className="align-content-start mt-2 mt-md-5">
-            <div className="beard-wax-text text-white">
-              <Col md={12}>
-                <p className="text-start text-center text-md-start section-text">
-                  "To become Sri Lanka’s most trusted and inspiring men’s
-                  grooming brand — empowering every man to wear his beard with
-                  confidence, pride, and personality."
-                </p>
-              </Col>
+          <Col md={7} className="d-flex flex-column">
+            <div className="beard-wax-text text-white h-100 d-flex flex-column">
+              <h1 className="text-uppercase text-center text-md-start mt-2 section-subtitle">
+                Vision
+              </h1>
+              <div className="mt-auto">
+                <Col md={12}>
+                  <p className="text-start text-center text-md-start section-text">
+                    "To become Sri Lanka's most trusted and inspiring men's
+                    grooming brand — empowering every man to wear his beard with
+                    confidence, pride, and personality."
+                  </p>
+                </Col>
+              </div>
             </div>
           </Col>
-          <Col
-            md={5}
-            style={{
-              backgroundImage: `url(${visionImage})`,
-              minHeight: "300px",
-              backgroundRepeat: "no-repeat",
-              backgroundPosition: "center",
-            }}
-          ></Col>
+          <Col>
+            <div className="product-image-container">
+              <img
+                src={visionImage}
+                alt="Vision Image"
+                className="img-fluid product-image"
+                height={150}
+              />
+            </div>
+          </Col>
         </Row>
 
         {/* Mission Section */}
-        <Row>
-          <h1 className="text-uppercase text-center text-md-end mt-5 section-subtitle">
-            Mission
-          </h1>
+        <Row className="mt-5">
           <Col md={7} className="align-content-start mt-2 d-md-none">
             <div className="beard-wax-text text-white ps-md-5">
+              <h1 className="text-uppercase text-center text-md-end mt-5 section-subtitle">
+                Mission
+              </h1>
               <Col md={12}>
                 <p className="text-start text-center text-md-end section-text">
                   We are on a mission to deliver high-quality, locally made
@@ -81,26 +93,33 @@ At Mr. Beard, we believe grooming is more than just style—it’s about confide
               </Col>
             </div>
           </Col>
-          <Col
-            md={5}
-            style={{
-              backgroundImage: `url(${missionImage})`,
-              minHeight: "440px",
-              backgroundRepeat: "no-repeat",
-              backgroundPosition: "center",
-            }}
-          ></Col>
-          <Col md={7} className="align-content-start mt-5 d-none d-md-block">
-            <div className="beard-wax-text text-white ps-md-5">
-              <Col md={12}>
-                <p className="text-start text-center text-md-end section-text">
-                  We are on a mission to deliver high-quality, locally made
-                  beard care products that solve real grooming problems. With
-                  natural ingredients, modern design, and genuine customer care,
-                  we help men grow healthier beards and stronger identities —
-                  one drop at a time.
-                </p>
-              </Col>
+          <Col>
+            <div className="product-image-container">
+              <img
+                src={missionImage}
+                alt="Vision Image"
+                className="img-fluid product-image"
+                height={150}
+              />
+            </div>
+          </Col>
+
+          <Col md={7} className="d-flex flex-column d-none d-md-block">
+            <div className="beard-wax-text text-white h-100 d-flex flex-column">
+              <h1 className="text-uppercase text-center text-md-end mt-2 section-subtitle">
+                Mission
+              </h1>
+              <div className="mt-auto">
+                <Col md={12}>
+                  <p className="text-start text-center text-md-end section-text">
+                    We are on a mission to deliver high-quality, locally made
+                    beard care products that solve real grooming problems. With
+                    natural ingredients, modern design, and genuine customer
+                    care, we help men grow healthier beards and stronger
+                    identities — one drop at a time.
+                  </p>
+                </Col>
+              </div>
             </div>
           </Col>
         </Row>
