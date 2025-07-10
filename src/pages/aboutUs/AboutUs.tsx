6630f9acf8abbc069fc98d094ceb810d4f5a5788
Senura Chamod <senoocoding@gmail.com>
2025-07-10 10:40:18 +0530

    MOD: Updated AboutUS Page to look exact as the design.

--- src/pages/aboutUs/AboutUs.tsx
+++ src/pages/aboutUs/AboutUs.tsx
@@ -8,47 +8,50 @@ import missionImage from "/src/assets/images/about/mission.png";
 const AboutUs: React.FC = () => {
   return (
     <div className="about-us">
+      {/* Move the about-us-header class to wrap the entire founder section */}
+      <div className="about-us-header">
+        <Container>
+          {/* About Us Header */}
+          <Row>
+            <Col xs={12} className="text-center mt-5">
+              <h1 className="section-title">Meet the Founder</h1>
+            </Col>
+          </Row>
+
+          {/* Title Section with Image */}
+          <Row className="mb-md-5 align-items-start">
+            <Col className="mb-4 mb-md-0 col-sm-auto">
+              <div className="product-image-container">
+                <img
+                  src={ownerImage}
+                  alt="The Company Founder"
+                  className="img-fluid product-image"
+                  width={450}
+                />
+              </div>
+            </Col>
+            <Col lg={6} md={8} sm={12} className="mt-lg-4">
+              <p className="founder-section-text text-md-start mt-md-5">
+                Dilshan T Amarasinghe is a dynamic entrepreneur, Visiting
+                Lecturer, and SME Business Consultant with a Entrepreneurship and
+                Business Management background from the University of Sri
+                Jayewardenepura. As the owner of four brands, he is dedicated to
+                empowering businesses and redefining men's grooming in Sri Lanka.
+                 <br />
+                <br />
+                At Mr. Beard, we believe grooming is more than just style—it's
+                about confidence, self-care, and making a difference. Join us in
+                transforming men's grooming while supporting a truly important
+                cause.
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+
       <Container>
-        <Row className="about-us-header">
-        {/* About Us Header */}
-        <Row>
-          <Col xs={12} className="text-center mt-5">
-            <h1 className="section-title">Meet the Founder</h1>
-          </Col>
-        </Row>
-
-        {/* Title Section with Image */}
-        <Row className="mb-md-5 align-items-start">
-          <Col className="mb-4 mb-md-0 col-sm-auto">
-            <div className="product-image-container">
-              <img
-                src={ownerImage}
-                alt="The Company Founder"
-                className="img-fluid product-image"
-                width={300}
-              />
-            </div>
-          </Col>
-          <Col lg={8} md={6} sm={12}>
-            <p className="section-text text-md-start mt-md-5">
-              Dilshan T Amarasinghe is a dynamic entrepreneur, Visiting
-              Lecturer, and SME Business Consultant with a Entrepreneurship and
-              Business Management background from the University of Sri
-              Jayewardenepura. As the owner of four brands, he is dedicated to
-              empowering businesses and redefining men’s grooming in Sri Lanka.
-               <br />
-              <br />
-              At Mr. Beard, we believe grooming is more than just style—it’s
-              about confidence, self-care, and making a difference. Join us in
-              transforming men’s grooming while supporting a truly important
-              cause.
-            </p>
-          </Col>
-        </Row>
-        </Row>
-
         {/* Vision Section */}
-        <Row className="mt-5">
+        <Row className="mt-0">
           <Col md={7} className="d-flex flex-column">
             <div className="beard-wax-text text-white h-100 d-flex flex-column">
               <h1 className="text-uppercase text-center text-md-start mt-2 section-subtitle">
@@ -66,7 +69,7 @@ const AboutUs: React.FC = () => {
             </div>
           </Col>
           <Col>
-            <div className="product-image-container">
+            <div className="product-image-container with-glow d-flex justify-content-center justify-content-md-end">
               <img
                 src={visionImage}
                 alt="Vision Image"
@@ -78,7 +81,7 @@ const AboutUs: React.FC = () => {
         </Row>
 
         {/* Mission Section */}
-        <Row className="mt-5">
+        <Row className="my-5">
           <Col md={7} className="align-content-start mt-2 d-md-none">
             <div className="beard-wax-text text-white ps-md-5">
               <h1 className="text-uppercase text-center text-md-end mt-5 section-subtitle">
@@ -96,10 +99,10 @@ const AboutUs: React.FC = () => {
             </div>
           </Col>
           <Col>
-            <div className="product-image-container">
+            <div className="product-image-container with-glow d-flex justify-content-center justify-content-md-start">
               <img
                 src={missionImage}
-                alt="Vision Image"
+                alt="Mission Image"
                 className="img-fluid product-image"
                 height={150}
               />
